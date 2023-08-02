6c0e1c60293f8bf2e68fa06cca80ac5c083d02d3
Pedro Kaleb! <73436445+LyeZinho@users.noreply.github.com>
2023-08-02 02:14:23 +0100

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Image from "next/image";
 const Header = () => {
   return (
-    <div className="min-h-full flex items-center justify-center py-16 px-4 sm:px-6 lg:px-8 ">
+    <div className="min-h-full flex items-center justify-center py-16 px-4 sm:px-6 lg:px-8 mt-10">
       <div className="flex flex-col items-center justify-center mt-6 space-x-10 ">
         <Image
           src="/assets/images/logo.png"

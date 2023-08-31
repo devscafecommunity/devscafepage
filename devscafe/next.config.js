2367f7ba5f73b768b008e5e7e84c76f8943b099b
Thiago Santos <t.mateus.santos2004@gmail.com>
2023-08-31 19:57:19 +0100

    feat

--- devscafe/next.config.js
+++ devscafe/next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
-    images: {
-        domains: ['raw.githubusercontent.com'],
-    },
+  reactStrictMode: true,
 }
 
 module.exports = nextConfig

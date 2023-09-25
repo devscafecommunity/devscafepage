d68704bbcc238789ab72158d4a8b0cd57bc5225e
Pedro Kaleb! <73436445+LyeZinho@users.noreply.github.com>
2023-09-25 19:47:12 +0100

    Update metadata & favicon

--- src/src/pages/_app.tsx
+++ src/src/pages/_app.tsx
@@ -1,12 +1,22 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 // Components
 import Navbar from '../../components/General/Navbar'
 
+let meta = {
+  title: "Dev's Café Community",
+  favicon: "/favicon.png",
+}
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div>
+      <Head>
+        <title>{meta.title}</title>
+        <link rel="icon" href={meta.favicon} />
+      </Head>
+      
       <Navbar />
       <div className="h-40"></div>
       <Component {...pageProps} />
